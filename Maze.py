78961e330d1f5641a1944db2a59f6a9e56315524
Journeyman08 <jakecoltman@sky.com>
2016-02-19 08:39:50 +0000

    Added basic maze implementation
    Encapsualte a numpy array that holds the value of every square

---
+++ Maze.py
@@ -0,0 +1,36 @@
+import numpy as np
+import random
+from matplotlib import pyplot as plt
+
+
+def _random_square(maze):
+    x, y = random.randint(1, maze.shape[0] - 2), random.randint(1, maze.shape[0] - 2)
+    return [x, y]
+
+
+def display_maze(maze):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = ax.pcolor(maze)
+    plt.show(block=False)
+
+
+class Maze:
+    def __init__(self, size):
+        self._generate_maze(size)
+        self.exitPos = _random_square()
+        self.maze[self.exitPos[0], self.exitPos[1]] = 2
+
+    def value_at_point(self, position):
+        return self.maze[position[0], position[1]]
+
+    def set_value_at_point(self, position, value):
+        self.maze[position[0], position[1]] = value
+
+    def _generate_maze(self, size):
+        base = np.zeros((size, size))
+        base[:, 0] = 1
+        base[0, :] = 1
+        base[-1, :] = 1
+        base[:, -1] = 1
+        self.maze = base
