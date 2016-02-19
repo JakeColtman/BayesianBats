db0c9940d359a455b4bc5c54de8c8984e6b4df44
Journeyman08 <jakecoltman@sky.com>
2016-02-19 18:32:45 +0000

    Added failing test for brain giving failing direction
    Brain should always provide a valid choice for the robot

--- Tests/Test_Brain.py
+++ Tests/Test_Brain.py
@@ -17,5 +17,11 @@ class TestBrainSetup(unittest.TestCase):
         newLength = len(brain.informationCache)
         self.assertEqual(prevLength + 1, newLength)
 
+    def test_brain_gives_a_valid_direction(self):
+        maze = Maze(3)
+        brain = Brain(maze)
+        direction = brain.determine_direction()
+        self.assertTrue(direction in ["left", "right", "up", "down"])
+        
 if __name__ == '__main__':
     unittest.main()
