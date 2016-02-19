8d3e0e3b27e5c57233bf2bc61085191c8a2157ab
Journeyman08 <jakecoltman@sky.com>
2016-02-19 09:02:23 +0000

    Formatting

--- Tests/Test_RoboBat.py
+++ Tests/Test_RoboBat.py
@@ -5,25 +5,25 @@ from Maze import Maze
 
 class RobotCreationAndMove(unittest.TestCase):
     def test_hardcoded_creation(self):
-        robotPos = [1, 4]
+        robot_pos = [1, 4]
         maze = Maze(7)
-        bat = RoboBat(maze, robotPos)
-        self.assertEqual(3, maze.value_at_point(robotPos))
+        bat = RoboBat(maze, robot_pos)
+        self.assertEqual(3, maze.value_at_point(robot_pos))
 
     def test_robot_move_updates_maze(self):
         maze = Maze(7)
-        robotPos = [1, 4]
-        bat = RoboBat(maze, robotPos)
+        robot_pos = [1, 4]
+        bat = RoboBat(maze, robot_pos)
         bat.move_in_direction("LEFT")
-        self.assertFalse(maze.value_at_point(robotPos) == 3)
+        self.assertFalse(maze.value_at_point(robot_pos) == 3)
         self.assertTrue(maze.value_at_point(bat.pos) == 3)
 
     def test_robot_move_changes_robotpos(self):
         maze = Maze(7)
-        robotPos = [1, 4]
-        bat = RoboBat(maze, robotPos)
+        robot_pos = [1, 4]
+        bat = RoboBat(maze, robot_pos)
         bat.move_in_direction("LEFT")
-        robotPos != bat.pos
+        robot_pos != bat.pos
 
 
 if __name__ == '__main__':
