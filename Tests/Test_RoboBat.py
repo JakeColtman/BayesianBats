cb19c676192eb7b14753557706cf52ac51c72623
Journeyman08 <jakecoltman@sky.com>
2016-02-19 18:08:06 +0000

    Null test for ability to see
    Surrounded by walls should report zero

--- Tests/Test_RoboBat.py
+++ Tests/Test_RoboBat.py
@@ -33,5 +33,15 @@ class RobotCreationAndMove(unittest.TestCase):
         self.assertEqual(bat.pos, [2, 4])
 
 
-if __name__ == '__main__':
-    unittest.main()
+class RobotLooking(unittest.TestCase):
+    def test_next_to_wall_returns_zero(self):
+        maze = Maze(3)
+        robot_pos = [1, 1]
+        bat = RoboBat(maze, robot_pos)
+        self.assertEqual(bat.distance_to_objection_in_direction("left"), 0)
+        self.assertEqual(bat.distance_to_objection_in_direction("down"), 0)
+        self.assertEqual(bat.distance_to_objection_in_direction("up"), 0)
+        self.assertEqual(bat.distance_to_objection_in_direction("right"), 0)
+
+        if __name__ == '__main__':
+            unittest.main()
